b9a7ad74b01f5be94afc89b1e701d55b37613e67
Sebastian <s.rubacha@actum.games>
2024-05-21 23:42:44 +0200

    Initial Project Files

---
+++ Source/GAS_ARPG/GAS_ARPG.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GAS_ARPG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAS_ARPG, "GAS_ARPG" );
